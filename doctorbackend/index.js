6459d86c4ef8add41b4ad79c31e2683a625643c4
sagarkashyap9835 <vidyasagar983531@gmail.com>
2025-10-01 08:53:36 +0530

    gghhff

--- doctorbackend/index.js
+++ doctorbackend/index.js
@@ -17,7 +17,7 @@ const allowedOrigins = [
   "http://localhost:5174",
   "http://localhost:5175",
   "https://frontend-doctor.onrender.com",
-  // "https://dashboard-uahn.onrender.com"
+   "https://admin-doctor-g6yq.onrender.com"
 ];
 
 app.use(
