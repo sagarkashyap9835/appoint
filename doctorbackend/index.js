be630f6cda5f6cfaabc8caf1cbf03a5f9ceaddd5
sagarkashyap9835 <vidyasagar983531@gmail.com>
2026-05-28 09:38:06 +0530

    gsdfgd

--- doctorbackend/index.js
+++ doctorbackend/index.js
@@ -16,8 +16,10 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5175",
-  "https://frontend-doctor.onrender.com",
-   "https://admin-doctor-g6yq.onrender.com"
+  "https://appointmentfrontend-vsii.onrender.com",
+  "https://appointmentfrontend-vsii.onrender.com/",
+  "https://appontmentadmin.onrender.com",
+  "https://appontmentadmin.onrender.com/"
 ];
 
 app.use(
